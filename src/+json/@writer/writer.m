669572551694e0eea8b9e6aa5cf74c860e48ed24
Jim Hokanson <jim.hokanson@gmail.com>
2016-07-23 21:17:49 -0400

    starting writer

---
+++ src/+json/@writer/writer.m
@@ -0,0 +1,21 @@
+classdef writer
+    %
+    %   Class:
+    %   json.writer
+    
+    properties
+        buffer
+        cur_index
+    end
+    
+    methods
+        function obj = writer()
+            
+        end
+        function addArray(obj,array)
+            
+        end
+    end
+    
+end
+
