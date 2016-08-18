57a95be525da7d0a0e8b633e7e1bf07442b4a7a1
Jim Hokanson <jim.hokanson@gmail.com>
2016-08-17 22:06:40 -0400

    Updated documentation on compiling

--- src/c_code/turtle_json_mex.c
+++ src/c_code/turtle_json_mex.c
@@ -3,11 +3,25 @@
  *      
  //must install openmp when installing tdm-gcc
  
- //Windows
+ //Windows - requires 2015b or newer - although with some work
+ //could probably get working on older versions, 
+ //
+ //At some point it would be nice to wrap this in a function with some
+ //additional logic tests
+ 
+ 1) Install TDM-GCC with openmp
+ http://tdm-gcc.tdragon.net/
+ 2) The matlab TDM-GCC entry on the file exchange might be needed, not sure
+ https://www.mathworks.com/matlabcentral/fileexchange/52848-matlab-support-for-the-mingw-w64-c-c++-compiler-from-tdm-gcc
+ 3) Might need to run:  
  setenv('MW_MINGW64_LOC','C:\TDM-GCC-64')
-
+ 4) Make the mex call
  mex CFLAGS="$CFLAGS -std=c11 -fopenmp -mavx" LDFLAGS="$LDFLAGS -fopenmp" COPTIMFLAGS="-O3 -DNDEBUG" turtle_json_mex.c turtle_json_main.c turtle_json_post_process.c -O -v
 
+ 
+ 
+ //Mac
+ //This required installing gcc ..., couldn't get openmp working with xcode
  mex CC='/usr/local/Cellar/gcc6/6.1.0/bin/gcc-6' CFLAGS="$CFLAGS -std=c11 -fopenmp -mavx" LDFLAGS="$LDFLAGS -fopenmp" COPTIMFLAGS="-O3 -DNDEBUG" turtle_json_mex.c turtle_json_main.c turtle_json_post_process.c -O -v
 
  *
