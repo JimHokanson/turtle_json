b6e6e4fda9f63bb69639d2b31b3f76e8112d3056
Jim Hokanson <jim.hokanson@gmail.com>
2016-12-27 17:43:45 -0500

    working on getting integrated examples

--- src/+json/+utils/@examples/examples.m
+++ src/+json/+utils/@examples/examples.m
@@ -7,26 +7,42 @@ classdef examples
     
     properties (Constant)
         FILE_LIST = {
-            'big.json'};
+            'big.json' %1
+            };
         DRIVE_PATH = 'https://drive.google.com/drive/folders/0B7to9gBdZEyGMExwTFA0ZWh1OTA?usp=sharing';
         
         %Apparently we can't download directly from Google drive anymore :/
+        %
+        %   Actually, this may be possible, with a bit of work ...
+        %
         %https://gsuiteupdates.googleblog.com/2015/08/deprecating-web-hosting-support-in.html
         %http://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
     end
     
     methods (Static)
-        function getFilePath(file_name_or_index,ensure_exists)
-            
-        end
-        function bin_path = getBinRoot()
+        function file_path = getFilePath(file_name_or_index)
             %
-            %   json.utils.examples.getBinRoot()
+            %   json.utils.examples.getFilePath()
+            
+            root_path = json.utils.examples.getExamplesRoot();
+            if ischar(file_name_or_index)
+                file_name = file_name_or_index;
+            else
+                file_name = json.utils.examples.FILE_LIST{file_name_or_index};
+            end
+            file_path = fullfile(root_path,file_name);
+        end
+        function bin_path = getExamplesRoot()
+            %
+            %   json.utils.examples.getExamplesRoot()
             
             root_path = fileparts(json.sl.stack.getPackageRoot());
 
             bin_path = fullfile(root_path,'bin'); 
         end
+%         function downloadFile(file_name)
+%             
+%         end
     end
     
 end
--- src/+json/+utils/getBinRoot.m
+++
@@ -1,9 +0,0 @@
-function bin_path = getBinRoot()
-%
-%   bin_path = json.utils.getBinRoot()
-
-root_path = fileparts(json.sl.stack.getPackageRoot());
-
-bin_path = fullfile(root_path,'bin');
-
-end
