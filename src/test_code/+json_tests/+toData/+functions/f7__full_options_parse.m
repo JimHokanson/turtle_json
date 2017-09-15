08acf36f3576bfb9d395ac52ff8fbe7a77bff267
Jim Hokanson <jim.hokanson@gmail.com>
2017-09-15 08:14:12 -0400

    added tests

--- src/test_code/+json_tests/+toData/+functions/f7__full_options_parse.m
+++ src/test_code/+json_tests/+toData/+functions/f7__full_options_parse.m
@@ -2,7 +2,14 @@ function f7__full_options_parse()
 
 %Invalid option testing
 %-----------------------------------------------------------------------
-
+%Numeric not boolean ...
+JSON_STR = '[[1,2,3],[4,5,6]]';
+try
+data = json.parse(JSON_STR,'max_numeric_collapse_depth',true);
+%Throw error here
+catch ME
+    %Throw error if not correct
+end
 
 %Column Majors
 %-----------------------------------------------------------------------
@@ -32,11 +39,29 @@ end
 
 %Max numeric collapse depth
 %------------------------------------------------------------------------
+%NOT YET IMPLEMENTED
+% JSON_STR = '[[1,2,3],[4,5,6]]';
+% data = json.parse(JSON_STR,'max_numeric_collapse_depth',0);
+% d2 = {[1;2;3],[4;5;6]};
+% if ~isequal(data,d2)
+%     error('Failed to properly parse nd-array using max_numeric_collapse_depth = 1');
+% end
+
 JSON_STR = '[[1,2,3],[4,5,6]]';
-data = json.parse(JSON_STR,'max_numeric_collapse_depth',true);
-if ~isequal(data,[1,2,3;4,5,6])
-    error('Failed to properly parse nd-array using row-major order');
+data = json.parse(JSON_STR,'max_numeric_collapse_depth',1);
+d2 = {[1;2;3],[4;5;6]};
+if ~isequal(data,d2)
+    error('Failed to properly parse nd-array using max_numeric_collapse_depth = 1');
 end
+
+JSON_STR = '[[1,2,3],[4,5,6]]';
+data = json.parse(JSON_STR,'max_numeric_collapse_depth',2);
+d2 = [1,4;2,5;3,6];
+if ~isequal(data,d2)
+    error('Failed to properly parse nd-array using max_numeric_collapse_depth = 2');
+end
+
+
 
 %   max_numeric_collapse_depth: default -1
 %
